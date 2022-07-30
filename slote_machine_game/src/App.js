8e38ae67cbc1c3ba93081997f2b38c9fa097757e
radhika <rvramayra1998@gmail.com>
2022-07-30 14:30:55 +0530

    add theme by using onMouseEnter or on MouseLeave

--- slote_machine_game/src/App.js
+++ slote_machine_game/src/App.js
@@ -5,12 +5,26 @@ import { Time } from './Components/Time';
 
 function App() {
   const [value,setValue] =useState(false);
-
+  const bg= "#282c34"
+  const t ="light"
+  const [color , setColor] =useState(bg)
+  const [text ,setText] = useState(t)
+const handleChangeColor=()=>{
+  let green ="#b3d4ff"
+  let nt = "Dark"
+  setColor(green)
+  setText(nt)
+}
+const handleCgangeColorAgin=()=>{
+  setText(t)
+  setColor(bg)
+}
   return (
     <div className="App">
-      <header className="App-header">
+      <button className="btn" onMouseEnter={handleChangeColor} onMouseLeave={handleCgangeColorAgin}> {text}</button>
+      <header  className="App-header" style={{backgroundColor:color}} >
       {value? <SloteData/> :<Time/> }
-      <button onClick={()=>setValue(prev=>!prev)}> {value ? "Let's check Time" : "Play Game"}</button>
+      <button className="button" onClick={()=>setValue(prev=>!prev)}> {value ? "Let's check Time" : "Play Game"}</button>
       </header>
     </div>
   );
