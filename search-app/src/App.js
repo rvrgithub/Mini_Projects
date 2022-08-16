12abc1286a57b0c397a9edefbbfda5e4eb5fb896
radhika <rvramayra1998@gmail.com>
2022-08-16 21:16:14 +0530

    update all

---
+++ search-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    {/* <h1 >Google</h1>
+  <input type="text" ></input> */}
+
+    </div>
+  );
+}
+
+export default App;
